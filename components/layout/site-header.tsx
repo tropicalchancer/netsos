8eef3495a23b3b4cc8aee800f7c449b8fea6565e
Chance <mcallisterchance@gmail.com>
2024-12-19 16:22:16 -0300

    fix navigation problem

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background">
-      <div className="container flex h-16 items-center justify-between">
+      <div className="container flex h-16 items-center justify-between px-4 md:px-6 lg:px-8">
         <div className="flex items-center gap-6">
           <Link href="/" className="font-bold">
             netso villages
