1f69853fbdff0fa862ed30cf1c9bb208168532ef
Chance <mcallisterchance@gmail.com>
2025-01-14 15:17:51 +0530

    fix production error

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,11 +1,16 @@
 'use client'
 
-import MapboxMap from '@/components/map/map'
+import dynamic from 'next/dynamic'
+
+// Import MapboxMap dynamically to prevent SSR issues
+const MapboxMap = dynamic(() => import('@/components/map/map'), {
+  ssr: false
+})
 
 export default function MapPage() {
   return (
     <div className="h-[calc(100vh-4rem)]">
-      <MapboxMap className="h-full" />
+      <MapboxMap />
     </div>
   )
 }
